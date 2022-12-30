c9c654213736cfe10f9c4974d223cb0a675d31c8
Alex Curran <alexcurran.va@gmail.com>
2022-12-30 16:24:43 -0600

    Recursive solution

--- linked-list/linked-list-practice/delete-value.js
+++ linked-list/linked-list-practice/delete-value.js
@@ -20,34 +20,72 @@ c.next = d;
 
 // a -> b -> c -> d
 
+// /**
+//  * solution 1 -- iterative
+//  * time: O(n)
+//  * space: O(1)
+//  * 
+//  * @param {ListNode} head 
+//  * @param {string} targetValue
+//  * @returns {ListNode} head of the modified list
+//  */ 
+// function deleteValue(head, targetValue) {
+//     // edge case where the beginning head needs to be deleted
+//     if (head.value === targetValue) return head.next;
+
+//     let previousNode = null;
+//     let currentNode = head;
+
+//     while (currentNode !== null) {
+//         if (currentNode.value === targetValue) {
+//             // delete node
+//             previousNode.next = currentNode.next;
+//         }
+//         // move previous pointer
+//         previousNode = currentNode;
+//         // move current pointer
+//         currentNode = currentNode.next;
+//     }
+
+//     return head;
+// }
+
 /**
- * solution 1 -- iterative
+ * solution 2 -- recursive
  * time: O(n)
- * space: O(1)
+ * space: O(n)
  * 
  * @param {ListNode} head 
  * @param {string} targetValue
  * @returns {ListNode} head of the modified list
  */ 
 function deleteValue(head, targetValue) {
-    // edge case where the beginning head needs to be deleted
     if (head.value === targetValue) return head.next;
 
-    let previousNode = null;
-    let currentNode = head;
-
-    while (currentNode !== null) {
-        if (currentNode.value === targetValue) {
-            // delete node
-            previousNode.next = currentNode.next;
-        }
-        // move previous pointer
-        previousNode = currentNode;
-        // move current pointer
-        currentNode = currentNode.next;
-    }
+    recurse(head, null, targetValue);
 
     return head;
+}
+
+/**
+ * @param {ListNode} currentNode 
+ * @param {ListNode|null} previousNode 
+ * @param {string} targetValue 
+ * @returns {void}
+ */
+function recurse(currentNode, previousNode, targetValue) {
+    // base cases
+    if (currentNode === null) return;
+
+    if (currentNode.value === targetValue) {
+        // delete current node
+        previousNode.next = currentNode.next;
+
+        return;
+    }
+
+    // recursive case
+    recurse(currentNode.next, currentNode, targetValue);
 }
 
 /**
@@ -62,5 +100,5 @@ function printList(head) {
     return printList(head.next);
 }
 
-const newListHead = deleteValue(a, "a"); // returns a -> b -> d
+const newListHead = deleteValue(a, "d"); // returns a -> b -> d
 printList(newListHead);
