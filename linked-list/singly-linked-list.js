c91f8b5ee16c5db5bf84c6c502ee09ab2d18f092
Alex Curran <alexcurran.va@gmail.com>
2025-08-28 17:06:54 -0500

    Append method

--- linked-list/singly-linked-list.js
+++ linked-list/singly-linked-list.js
@@ -15,4 +15,25 @@ class LinkedList {
         this.head = null; // start
         this.length = 0;
     }
+
+    /**
+     * @param {number} value
+     */
+    append(value) {
+        const newNode = new ListNode(value);
+
+        if (!this.head) {
+            // empty list
+            this.head = newNode;
+        } else {
+            // pre-existing list, add to end
+            let current = this.head;
+
+            while (current.next !== null) {
+                current = current.next;
+            }
+
+            current.next = newNode; // link at the end
+        }
+    }
 }
