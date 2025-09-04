ba84210297a78f33ee7ed896118ddb6eb8ff5693
Alex Curran <alexcurran.va@gmail.com>
2025-09-04 16:38:08 -0500

    Recursive solution

--- linked-list/linked-list-practice/lc-style-problems/reverse-list.js
+++ linked-list/linked-list-practice/lc-style-problems/reverse-list.js
@@ -7,27 +7,47 @@ myLinkedList.append(3);
 myLinkedList.append(4);
 myLinkedList.append(5);
 
+// /**
+//  * solution 1 -- iterative
+//  * time: O(n)
+//  * space: O(1)
+//  *
+//  * @param {ListNode} head
+//  * @returns {ListNode}
+//  */
+// function reverseList(head) {
+//     let previous = null;
+//     let current = head;
+
+//     while (current !== null) {
+//         // save next pointer before breaking chain and re-assigning
+//         const temp = current.next;
+//         current.next = previous; // reverse link
+//         previous = current; // move forward
+//         current = temp; // move forward
+//     }
+
+//     return previous;
+// }
+
 /**
- * solution 1 -- iterative
+ * solution 2 -- recursive
  * time: O(n)
- * space: O(1)
+ * space: O(n)
  *
  * @param {ListNode} head
  * @returns {ListNode}
  */
-function reverseList(head) {
-    let previous = null;
-    let current = head;
-
-    while (current !== null) {
-        // save next pointer before breaking chain and re-assigning
-        const temp = current.next;
-        current.next = previous; // reverse link
-        previous = current; // move forward
-        current = temp; // move forward
+function reverseList(head, previous = null) {
+    if (head === null) {
+        return previous;
     }
 
-    return previous;
+    const temp = head.next;
+    // re-link pointer
+    head.next = previous;
+
+    return reverseList(temp, head);
 }
 
 console.log(reverseList(myLinkedList.head)); // [5, 4, 3, 2, 1]
