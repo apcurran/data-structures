5e2209dc7b737b581a968d374538a7f3c3f6f735
Alex Curran <alexcurran.va@gmail.com>
2025-09-04 13:57:21 -0500

    Hashset solution

--- linked-list/linked-list-practice/lc-style-problems/remove-duplicates.js
+++ linked-list/linked-list-practice/lc-style-problems/remove-duplicates.js
@@ -7,10 +7,44 @@ myLinkedList1.append(1);
 myLinkedList1.append(3);
 myLinkedList1.append(2);
 
+// /**
+//  * solution 1 -- nested loops
+//  * time: O(n^2)
+//  * space: O(1)
+//  *
+//  * @param {ListNode} head
+//  * @returns {ListNode|null}
+//  */
+// function removeDuplicates(head) {
+//     if (!head) return null;
+
+//     let current = head;
+
+//     while (current !== null) {
+//         let runner = current; // reset for each new 'current' node
+
+//         while (runner.next !== null) {
+//             // is there a duplicate (where current equals runner?)
+//             if (runner.next.val === current.val) {
+//                 // remove node
+//                 runner.next = runner.next.next;
+//             } else {
+//                 // only move forward if a node was not removed
+//                 runner = runner.next;
+//             }
+//         }
+
+//         // move outer pointer
+//         current = current.next;
+//     }
+
+//     return head;
+// }
+
 /**
- * solution 1 -- nested loops
- * time: O(n^2)
- * space: O(1)
+ * solution 2 -- hashset
+ * time: O(n)
+ * space: O(n)
  *
  * @param {ListNode} head
  * @returns {ListNode|null}
@@ -18,23 +52,21 @@ myLinkedList1.append(2);
 function removeDuplicates(head) {
     if (!head) return null;
 
+    let seenValues = new Set();
     let current = head;
+    let previous = null;
 
     while (current !== null) {
-        let runner = current; // reset for each new 'current' node
-
-        while (runner.next !== null) {
-            // is there a duplicate (where current equals runner?)
-            if (runner.next.val === current.val) {
-                // remove node
-                runner.next = runner.next.next;
-            } else {
-                // only move forward if a node was not removed
-                runner = runner.next;
-            }
+        if (seenValues.has(current.val)) {
+            // handle duplicate found
+            // remove node
+            previous.next = current.next;
+        } else {
+            // handle no duplicate
+            seenValues.add(current.val);
+            previous = current;
         }
 
-        // move outer pointer
         current = current.next;
     }
 
