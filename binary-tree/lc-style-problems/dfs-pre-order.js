98c92286961c54e3ef40bbbda2b595669c8060ca
Alex Curran <alexcurran.va@gmail.com>
2025-09-12 15:34:36 -0500

    Recursive pre-order

---
+++ binary-tree/lc-style-problems/dfs-pre-order.js
@@ -0,0 +1,44 @@
+import { BinarySearchTree, TreeNode } from "../bst-class.js";
+
+/**
+ * solution 1 -- recursive
+ * time: O(n)
+ * space: O(n)
+ *
+ * @param {TreeNode} root
+ * @returns {array}
+ */
+function dfsPreOrder(root) {
+    let data = [];
+
+    /**
+     * @param {TreeNode} root
+     * @returns {void} -- side effect pushing to data array
+     */
+    function traverse(root) {
+        if (!root) {
+            return;
+        }
+
+        // add to results
+        data.push(root.value);
+        // check left and right
+        traverse(root.left);
+        traverse(root.right);
+    }
+
+    traverse(root); // invoke first to start recursive calls
+
+    return data;
+}
+
+let myBST = new BinarySearchTree();
+myBST.insert(47);
+myBST.insert(21);
+myBST.insert(76);
+myBST.insert(18);
+myBST.insert(27);
+myBST.insert(52);
+myBST.insert(82);
+
+console.log(dfsPreOrder(myBST.root)); // [47, 21, 18, 27, 76, 52, 82]
