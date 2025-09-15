0acc56dbb925495937912095864200833d23894b
Alex Curran <alexcurran.va@gmail.com>
2025-09-15 14:33:49 -0500

    Iterative solution for DFS pre-order

--- binary-tree/lc-style-problems/dfs-pre-order.js
+++ binary-tree/lc-style-problems/dfs-pre-order.js
@@ -32,6 +32,38 @@ function dfsPreOrder(root) {
     return data;
 }
 
+/**
+ * solution 2 -- iterative
+ * time: O(n)
+ * space: O(n)
+ *
+ * @param {TreeNode} root
+ * @returns {array}
+ */
+function dfsPreOrderIterative(root) {
+    if (!root) return [];
+
+    let stack = [root];
+    let results = [];
+
+    while (stack.length > 0) {
+        // remove node from stack
+        const current = stack.pop();
+        // add node value to results array
+        results.push(current.value);
+        // check for child nodes
+        if (current.right) {
+            stack.push(current.right);
+        }
+
+        if (current.left) {
+            stack.push(current.left);
+        }
+    }
+
+    return results;
+}
+
 let myBST = new BinarySearchTree();
 myBST.insert(47);
 myBST.insert(21);
@@ -42,3 +74,4 @@ myBST.insert(52);
 myBST.insert(82);
 
 console.log(dfsPreOrder(myBST.root)); // [47, 21, 18, 27, 76, 52, 82]
+console.log(dfsPreOrderIterative(myBST.root)); // [47, 21, 18, 27, 76, 52, 82]
