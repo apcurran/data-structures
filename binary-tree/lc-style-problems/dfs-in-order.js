a1f882c62da4fbbff48d905fe33f2ab6fbbc1f72
Alex Curran <alexcurran.va@gmail.com>
2025-09-15 15:51:37 -0500

    Recursive DFS in-order solution

--- binary-tree/lc-style-problems/dfs-in-order.js
+++ binary-tree/lc-style-problems/dfs-in-order.js
@@ -1,11 +1,35 @@
 import { BinarySearchTree, TreeNode } from "../bst-class.js";
 
 /**
+ * solution 1 -- recursive
+ * time: O(n)
+ * space: O(n)
  *
  * @param {TreeNode} root
  * @returns {number[]}
  */
-function dfsInOrder(root) {}
+function dfsInOrder(root) {
+    let results = [];
+
+    /**
+     * @param {TreeNode} root
+     * @returns {voic}
+     */
+    function traverse(root) {
+        if (root === null) return;
+
+        // check left child
+        traverse(root.left);
+        // add value to results
+        results.push(root.value);
+        // check right child
+        traverse(root.right);
+    }
+
+    traverse(root);
+
+    return results;
+}
 
 let myBST = new BinarySearchTree();
 myBST.insert(47);
@@ -16,4 +40,10 @@ myBST.insert(27);
 myBST.insert(52);
 myBST.insert(82);
 
-console.log(dfsInOrder(myBST.root)); // []
+//       47
+//     /    \
+//    21    76
+//   /  \   / \
+//  18  27 52  82
+
+console.log(dfsInOrder(myBST.root)); // [18, 21, 27, 47, 52, 76, 82] sorted values
